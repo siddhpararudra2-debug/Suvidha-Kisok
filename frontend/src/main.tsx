dc55fffd9a67fb13ffcab6c744bba85e5217a40a
siddhpararudra2-debug <siddhpararudra2@gmail.com>
2026-02-01 00:09:15 +0530

    Fix routing: use HashRouter for GitHub Pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import App from './App';
 import { store } from './store';
@@ -12,12 +12,12 @@ import './i18n';
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <ThemeProvider theme={theme}>
                     <CssBaseline />
                     <App />
                 </ThemeProvider>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
